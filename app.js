8c2a9f77db6b1ed36cc6e16908f56d84d085563b
Bayu Stiawan <bayu@stiawan.com>
2023-02-11 19:54:49 +0800

    Working set up

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { App, LogLevel } = require('@slack/bolt');
+require('dotenv').config();
 
 const actions = require('./incoming/actions');
 const { hello } = require('./incoming/message/messages');
@@ -10,7 +11,6 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: false,
-  appToken: process.env.SLACK_APP_TOKEN,
   logLevel: LogLevel.INFO,
   customRoutes: [
     {
@@ -29,8 +29,8 @@ const app = new App({
 app.message('hello', hello);
 
 // Listens to actions
-app.action({block_id: 'message_action', action_id: 'reply_message'}, actions.replyMessage);
-app.action({block_id: 'message_action', action_id: 'ignore_message'}, actions.ignoreMessage);
+app.action({ block_id: 'message_action', action_id: 'reply_message' }, actions.replyMessage);
+app.action({ block_id: 'message_action', action_id: 'ignore_message' }, actions.ignoreMessage);
 
 app.error((error) => {
   // Check the details of the error to handle cases where you should retry sending a message or stop the app
