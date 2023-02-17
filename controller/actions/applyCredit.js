2afae5a572ebce580be41ca678f534b242e73159
Bayu Stiawan <bayu@stiawan.com>
2023-02-17 11:24:35 +0800

    Update applyCredit.js

--- controller/actions/applyCredit.js
+++ controller/actions/applyCredit.js
@@ -22,12 +22,46 @@ module.exports = {
                 // View payload
                 view: {
                     type: 'modal',
-                    callback_id: 'apply_credit',
-                    private_metadata: JSON.stringify(private_metadata),
-                    title: modal.title('Apply Credit'),
-                    blocks: blocks.applyCreditBlocks(body.user.id, body.user.team_id),
-                    close: modal.close('Cancel'),
-                    submit: modal.submit('Apply'),
+                    // View identifier
+                    callback_id: 'view_1',
+                    title: {
+                        type: 'plain_text',
+                        text: 'Modal title'
+                    },
+                    blocks: [
+                        {
+                            type: 'section',
+                            text: {
+                                type: 'mrkdwn',
+                                text: 'Welcome to a modal with _blocks_'
+                            },
+                            accessory: {
+                                type: 'button',
+                                text: {
+                                    type: 'plain_text',
+                                    text: 'Click me!'
+                                },
+                                action_id: 'button_abc'
+                            }
+                        },
+                        {
+                            type: 'input',
+                            block_id: 'input_c',
+                            label: {
+                                type: 'plain_text',
+                                text: 'What are your hopes and dreams?'
+                            },
+                            element: {
+                                type: 'plain_text_input',
+                                action_id: 'dreamy_input',
+                                multiline: true
+                            }
+                        }
+                    ],
+                    submit: {
+                        type: 'plain_text',
+                        text: 'Submit'
+                    }
                 }
             });
             logger.info(new Date().toLocaleString('id-ID'), "applyCredit", "Show modal", show.ok)
