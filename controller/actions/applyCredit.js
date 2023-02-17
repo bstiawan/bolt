847451b993ff6d61b6e633150c27b8948203511d
Bayu Stiawan <bayu@stiawan.com>
2023-02-17 11:18:35 +0800

    Update applyCredit.js

--- controller/actions/applyCredit.js
+++ controller/actions/applyCredit.js
@@ -4,7 +4,7 @@ const blocks = require('../../view/blocks');
 
 module.exports = {
     applyCredit: async ({ client, body, ack, logger }) => {
-        logger.info("applyCredit", body.actions[0].action_id)
+        logger.info(new Date().toLocaleString('id-ID'), "applyCredit", body.actions[0].action_id)
         // logger.info(body)
 
         // Acknowledge the action
@@ -14,29 +14,29 @@ module.exports = {
             team_id: body.user.team_id
         };
 
-        await client.views.open({
-            // Pass a valid trigger_id within 3 seconds of receiving it
-            trigger_id: body.trigger_id,
-            // View payload
-            view: {
-                type: 'modal',
-                callback_id: 'apply_credit',
-                private_metadata: JSON.stringify(private_metadata),
-                title: modal.title('Apply Credit'),
-                blocks: [
-                    {
-                        "type": "section",
-                        "text": {
-                            "type": "mrkdwn",
-                            "text": "Now loading..."
-                        }
-                    }
-                ],
-                close: modal.close('Cancel'),
-                submit: modal.submit('Apply'),
-            }
-        });
-        logger.info("applyCredit", "Show modal")
-        // logger.info(result);
+        try {
+            logger.info(new Date().toLocaleString('id-ID'), "applyCredit", "Try")
+            const show = await client.views.open({
+                // Pass a valid trigger_id within 3 seconds of receiving it
+                trigger_id: body.trigger_id,
+                // View payload
+                view: {
+                    type: 'modal',
+                    callback_id: 'apply_credit',
+                    private_metadata: JSON.stringify(private_metadata),
+                    title: modal.title('Apply Credit'),
+                    blocks: blocks.applyCreditBlocks(body.user.id, body.user.team_id),
+                    close: modal.close('Cancel'),
+                    submit: modal.submit('Apply'),
+                }
+            });
+            logger.info(new Date().toLocaleString('id-ID'), "applyCredit", "Show modal", show.ok)
+            // logger.info(result);
+        }
+        catch (error) {
+            logger.error(error);
+        }
+        logger.info(new Date().toLocaleString('id-ID'), "applyCredit", "End")
+
     }
 }
