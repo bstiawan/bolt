4869fc663f34812eec511a6d1d75980c4c10edb6
Bayu Stiawan <bayu@stiawan.com>
2023-02-17 11:51:59 +0800

    Button in no credit message

--- controller/actions/applyCredit.js
+++ controller/actions/applyCredit.js
@@ -4,7 +4,7 @@ const blocks = require('../../view/blocks');
 
 module.exports = {
     applyCredit: async ({ client, body, ack, logger }) => {
-        logger.info(new Date().toLocaleString('id-ID'), "applyCredit", body.actions[0].action_id)
+        logger.info("applyCredit", body.actions[0].action_id)
         // logger.info(body)
 
         // Acknowledge the action
@@ -15,62 +15,26 @@ module.exports = {
         };
 
         try {
-            logger.info(new Date().toLocaleString('id-ID'), "applyCredit", "Try")
             const show = await client.views.open({
                 // Pass a valid trigger_id within 3 seconds of receiving it
                 trigger_id: body.trigger_id,
                 // View payload
                 view: {
                     type: 'modal',
-                    // View identifier
-                    callback_id: 'view_1',
-                    title: {
-                        type: 'plain_text',
-                        text: 'Modal title'
-                    },
-                    blocks: [
-                        {
-                            type: 'section',
-                            text: {
-                                type: 'mrkdwn',
-                                text: 'Welcome to a modal with _blocks_'
-                            },
-                            accessory: {
-                                type: 'button',
-                                text: {
-                                    type: 'plain_text',
-                                    text: 'Click me!'
-                                },
-                                action_id: 'button_abc'
-                            }
-                        },
-                        {
-                            type: 'input',
-                            block_id: 'input_c',
-                            label: {
-                                type: 'plain_text',
-                                text: 'What are your hopes and dreams?'
-                            },
-                            element: {
-                                type: 'plain_text_input',
-                                action_id: 'dreamy_input',
-                                multiline: true
-                            }
-                        }
-                    ],
-                    submit: {
-                        type: 'plain_text',
-                        text: 'Submit'
-                    }
+                    callback_id: 'apply_credit',
+                    private_metadata: JSON.stringify(private_metadata),
+                    title: modal.title('Apply Credit'),
+                    blocks: blocks.applyCreditBlocks(body.user.id, body.user.team_id),
+                    close: modal.close('Cancel'),
+                    submit: modal.submit('Apply'),
                 }
             });
-            logger.info(new Date().toLocaleString('id-ID'), "applyCredit", "Show modal", show.ok)
+            logger.info("applyCredit", "Show modal", show.ok)
             // logger.info(result);
         }
         catch (error) {
             logger.error(error);
         }
-        logger.info(new Date().toLocaleString('id-ID'), "applyCredit", "End")
 
     }
 }
--- view/blocks/noCreditMessageBlocks.js
+++ view/blocks/noCreditMessageBlocks.js
@@ -1,3 +1,5 @@
+const button = require('../button');
+
 module.exports = {
     noCreditMessageBlocks: (team_id) => {
         const blocks = [
@@ -10,20 +12,10 @@ module.exports = {
             },
             {
                 "type": "actions",
-                "block_id": "home_button",
+                "block_id": "team_action",
                 "elements": [
-                    {
-                        "type": "button",
-                        "style": "primary",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Go to home page",
-                            "emoji": true
-                        },
-                        "value": team_id,
-                        "action_id": "go_to_app_home",
-                        "url": `slack://app?team=${team_id}&id=${process.env.SLACK_APP_ID}&tab=home`
-                    }
+                    button.buyCreditButton(team_id),
+                    button.applyCreditButton(team_id)
                 ]
             }
         ]
