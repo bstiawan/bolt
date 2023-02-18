de69c415d6fa402523a12fdcf4f21fe2519c8122
Bayu Stiawan <bayu@stiawan.com>
2023-02-18 13:02:42 +0800

    Auth

--- controller/webhook/authRedirect.js
+++ controller/webhook/authRedirect.js
@@ -1,12 +1,35 @@
+const supabase = require('../../api/supabase');
+
 module.exports = {
-    authRedirect: (req, app) => {
+    authRedirect: async (req, res, app) => {
         console.log(req.url)
 
-        const data = req.body;
+        const data = req.params;
 
         console.log(data);
 
-        // Check signing secret first
+        // Exchange oauth code for access token
+        const access = app.client.oauth.v2.access({
+            // The code param is returned via the redirect from the Slack OAuth flow
+            code: data.code,
+            // This is the same as verification token in your App config
+            client_id: process.env.SLACK_CLIENT_ID,
+            client_secret: process.env.SLACK_CLIENT_SECRET
+        })
 
+        // If access is ok, store token to database
+        if (access.ok) {
+            // Store the token in a database
+            await supabase.upsertTeam({
+                team_id: access.team.id,
+                access_token: access.access_token,
+                app_id: access.app_id,
+                bot_user_id: access.bot_user_id,
+                team_name: access.team.name,
+            });
+
+            res.redirect(`https://slack.com/app_redirect?app=${process.env.SLACK_APP_ID}&team=${access.team.id}`)
+        }
     }
+
 }
