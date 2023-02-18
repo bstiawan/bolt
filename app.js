9dcb5d6726296112658f00414c5a061684da58b3
Bayu Stiawan <bayu@stiawan.com>
2023-02-18 23:44:30 +0800

    Update app.js

--- app.js
+++ app.js
@@ -33,50 +33,7 @@ const app = new App({
   clientSecret: process.env.SLACK_CLIENT_SECRET,
   stateSecret: 'whaaat',
   scopes: ['app_mentions:read', 'channels:history', 'chat:write', 'groups:history', 'im:history', 'mpim:history', 'users:read'],
-  installationStore: {
-    storeInstallation: async (installation) => {
-      // Bolt will pass your handler an installation object
-      // Change the lines below so they save to your database
-      if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
-        // handle storing org-wide app installation
-        console.log("storeInstallation", "isEnterpriseInstall", installation.enterprise.id)
-        // return await database.set(installation.enterprise.id, installation);
-      }
-      if (installation.team !== undefined) {
-        // single team app installation
-        return await supabase.uploadFile(installation);
-      }
-      throw new Error('Failed saving installation data to installationStore');
-    },
-    fetchInstallation: async (installQuery) => {
-      // Bolt will pass your handler an installQuery object
-      // Change the lines below so they fetch from your database
-      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-        // handle org wide app installation lookup
-        console.log("fetchInstallation", "isEnterpriseInstall", installQuery.enterpriseId)
-        // return await database.get(installQuery.enterpriseId);
-      }
-      if (installQuery.teamId !== undefined) {
-        // single team app installation lookup
-        return await supabase.fetchFile(installQuery.teamId);
-      }
-      throw new Error('Failed fetching installation');
-    },
-    deleteInstallation: async (installQuery) => {
-      // Bolt will pass your handler  an installQuery object
-      // Change the lines below so they delete from your database
-      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
-        // org wide app installation deletion
-        console.log("deleteInstallation", "isEnterpriseInstall", installQuery.enterpriseId)
-        // return await database.delete(installQuery.enterpriseId);
-      }
-      if (installQuery.teamId !== undefined) {
-        // single team app installation deletion
-        return await supabase.deleteFile(installQuery.teamId);
-      }
-      throw new Error('Failed to delete installation');
-    }
-  },
+  installationStore: new FileInstallationStore(),
 });
 
 (async () => {
