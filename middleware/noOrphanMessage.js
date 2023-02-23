184cefd83844202dbd4ef235a701b5ba1f9ccb87
Bayu Stiawan <bayu@stiawan.com>
2023-02-23 23:12:45 +0800

    Update noOrphanMessage.js

--- middleware/noOrphanMessage.js
+++ middleware/noOrphanMessage.js
@@ -1,7 +1,10 @@
 module.exports = {
     noOrphanMessage: async ({ event, logger, next }) => {
-        if (event.thread_ts || event.channel_type === 'im') {
-            // logger.info('Message is not a bot message');
+        if (event.thread_ts) {
+            logger.info('Message is a thread reply');
+            await next();
+        } else if (event.channel_type === 'im') {
+            logger.info('Message is a direct message');
             await next();
         }
     }
